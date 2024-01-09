ace4967a78c941623bf8f02f180e09a1e9c638fa
Mark Garrigan <mrkgrrgn@gmail.com>
2024-01-09 06:11:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ⚡️spry.nvim Installation script
+
+Learn how to use this script at https://github.com/sprynvim/spry.nvim.
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing spry..."
+
+spry_url=https://raw.githubusercontent.com/sprynvim/spry.nvim/main/spry.lua
+init_dir=~/.config/nvim
+init_file=$init_dir/init.lua
+mkdir -p $init_dir
+touch $init_file
+
+lines=('-- Added by spry installation' 'require("spry")' '-- End of spry installation')
+printf '%s\n' "${lines[@]}" | cat - $init_file > /tmp/init.lua.tmp
+mv /tmp/init.lua.tmp $init_file
+
+lua_dir=$init_dir/lua
+mkdir -p $lua_dir
+cd $lua_dir
+curl -o $lua_dir/spry.lua -sL $spry_url
+sleep 2
+
+echo -ne "All done.\033[0K\r"
+echo "Getting Started"
+echo "Read the docs at https://github.com/sprynvim/spry.nvim"
